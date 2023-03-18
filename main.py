072b468bb87c31559d72cc05b2b4341e5964b19b
ImDarkTom <darktomg21@gmail.com>
2023-03-18 00:18:56 +0000

    created base browser

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
-import urllib
-import validators
 
 WORKINGDIR = os.getcwd()
 
